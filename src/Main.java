51eebc3d6d1aacede91f73b19402386f70543f82
Camille Boinaud <camille.boinaud@gmail.com>
2015-03-29 17:25:51 +0200

    Ajout des exemples avec 3 niveaux (gris/couleurs)

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,15 @@ public class Main {
 
     public static void main(String[] args){
 
-        String filename1 = "house";
+        /*String filename1 = "img/pimen";
         ImageRGB img1 = new ImageRGB(filename1);
-        System.out.println(img1.histogram.length);
-        img1.conversion(16);
-        img1.writeBitmap();
+        img1.conversion(3);
+        img1.writeBitmap();*/
 
 
-        /*String filename2 = "horse_modified";
+        String filename2 = "img/mandr_lumi";
         ImageGrayScale img2 = new ImageGrayScale(filename2);
-        img2.conversion(16);
-        img2.writeBitmap();*/
+        img2.conversion(3);
+        img2.writeBitmap();
     }
 }
